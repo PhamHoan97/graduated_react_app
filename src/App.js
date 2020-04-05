388b8f4eba2093a5935a71ab67c95182ed707007
TranVietHuy <huytran161297@gmail.com>
2020-04-06 05:00:11 +0700

    validate for system login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
-// import Demoviewbpmn from './Demo/Demoviewbpmn';
-// import Demoeditprocess from './Demo/Demoeditprocess';
-import Democreateprocess from './Demo/Democreateprocess';
+import { BrowserRouter as Router } from "react-router-dom";
+import ListUrl from './Routes/ListUrl';
 
 function App() {
   return (
-    <div className="App">
-          {/* <Demoviewbpmn></Demoviewbpmn>
-          <Demoeditprocess></Demoeditprocess> */}
-          <Democreateprocess></Democreateprocess>
-    </div>
+    <Router>
+        <div className="App">
+          <ListUrl></ListUrl>
+        </div>
+    </Router>
   );
 }
 
