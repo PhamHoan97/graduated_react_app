762b36e6b7e2842c3c63ef2d1db3b2f23f1a5a9d
TranVietHuy <huytran161297@gmail.com>
2020-06-13 12:02:18 +0700

    search process in system

--- src/System/Component/Dashboard/Dashboard.js
+++ src/System/Component/Dashboard/Dashboard.js
@@ -3,8 +3,29 @@ import MenuHorizontal from "../Menu/MenuHorizontal";
 import MenuVerticalDashboard from "../Menu/MenuVerticalDashboard";
 import CompanyContainer from "../../Container/Dashboard/CompanyContainer";
 import ProcessDashboardContainer from "../../Container/Dashboard/ProcessDashboardContainer";
+import {getTextSearchProcess}  from "../../Action/Dashboard/Process/Index"; 
+import {connect} from "react-redux";
+import * as actions from '../../../Alert/Action/Index';
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      search: '',  
+    }
+  }
+  
+  handleSearch = event => {
+    var searchValue = event.target.value;
+    this.setState({search: searchValue});
+  }
+
+  searchProcesses = (e) => {
+    e.preventDefault(); 
+    var search = this.state.search;
+    this.props.getTextSearchProcess(search);
+  }
 
   render() {
     return (
@@ -16,7 +37,7 @@ class Dashboard extends Component {
             <div className="section__content section__content--p30">
               <div className="container-fluid">
                 <CompanyContainer/>
-                <div className="row text-left">
+                <div className="row text-left" style={{height:"36px"}}>
                   <div className="col-md-12">
                     <h3 className="title-5 m-b-35 dashboard__title--process">
                       Danh sách quy trình
@@ -24,6 +45,23 @@ class Dashboard extends Component {
                   </div>
                 </div>
                 <div className="row">
+                  <div className="col-md-12">
+                    <div className="table-data__tool">
+                      <div className="table-data__tool-left">
+                    
+                      </div>
+                      <div className="table-data__tool-right">
+                          <div className="rs-select2--light-search-company">
+                              <form className="form-search-employee">
+                                  <input className="form-control" onChange={this.handleSearch} placeholder="Tìm kiếm quy trình ..." />
+                                  <button className="employee-btn--search__process" type="button" onClick={(e) => this.searchProcesses(e)}><i className="zmdi zmdi-search"></i></button>
+                              </form>
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row" style={{marginTop:"5px"}}> 
                   <div className="col-md-12">
                      <ProcessDashboardContainer/>
                   </div>
@@ -47,4 +85,22 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+const mapStateToProps = (state, ownProps) => {
+  return {
+
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+      getTextSearchProcess: (textSearch) => {
+          dispatch(getTextSearchProcess(textSearch))
+      },
+      showAlert: (properties) => {
+        dispatch(actions.showMessageAlert(properties))
+      }
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Dashboard);
+
